54925b63b7265a481eecb911580f374ddeb54a1a
nihatsirinn <nihatsirin23@gmail.com>
2025-06-19 15:59:56 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,31 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signInWithPhoneNumber,
+  RecaptchaVerifier
+} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  query,
+  orderBy,
+  onSnapshot,
+  serverTimestamp
+} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCu4KtTBI-pEH2a9AzhKN1pKfd9u6YtkNs",
   authDomain: "diamond-chat-7c95d.firebaseapp.com",
   projectId: "diamond-chat-7c95d",
-  storageBucket: "diamond-chat-7c95d.firebasestorage.app",
+  storageBucket: "diamond-chat-7c95d.appspot.com",
   messagingSenderId: "920115621429",
   appId: "1:920115621429:web:6aac8db7cb7a8b6d46cb7c",
   measurementId: "G-MJ1VY7741G"
@@ -16,4 +35,21 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-export { auth, db };
+export {
+  auth,
+  db,
+  collection,
+  addDoc,
+  query,
+  orderBy,
+  onSnapshot,
+  serverTimestamp,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signInWithPhoneNumber,
+  RecaptchaVerifier
+};
