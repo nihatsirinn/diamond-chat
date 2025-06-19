f1237f5c43db4cdd4ded87ba66199b9d1d02df78
nihatsirinn <nihatsirin23@gmail.com>
2025-06-19 16:34:06 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,57 +1,15 @@
-import { initializeApp } from "firebase/app";
-import {
-  getAuth,
-  GoogleAuthProvider,
-  signInWithPopup,
-  signOut,
-  onAuthStateChanged,
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-  signInWithPhoneNumber,
-  RecaptchaVerifier
-} from "firebase/auth";
-
-import {
-  getFirestore,
-  collection,
-  addDoc,
-  query,
-  orderBy,
-  onSnapshot,
-  serverTimestamp
-} from "firebase/firestore";
-
+// Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyCu4KtTBI-pEH2a9AzhKN1pKfd9u6YtkNs",
-  authDomain: "diamond-chat-7c95d.firebaseapp.com",
-  projectId: "diamond-chat-7c95d",
-  storageBucket: "diamond-chat-7c95d.appspot.com",
-  messagingSenderId: "920115621429",
-  appId: "1:920115621429:web:6aac8db7cb7a8b6d46cb7c",
-  measurementId: "G-MJ1VY7741G"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_MSG_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-const provider = new GoogleAuthProvider();
+firebase.initializeApp(firebaseConfig);
 
-export {
-  auth,
-  db,
-  collection,
-  addDoc,
-  query,
-  orderBy,
-  onSnapshot,
-  serverTimestamp,
-  GoogleAuthProvider,
-  signInWithPopup,
-  signOut,
-  onAuthStateChanged,
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-  signInWithPhoneNumber,
-  RecaptchaVerifier,
-  provider
-};
+const auth = firebase.auth();
+const db = firebase.firestore();
+const provider = new firebase.auth.GoogleAuthProvider();
